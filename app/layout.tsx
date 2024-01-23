aa8cd1b37d68e4e5cec4500c8cbcc1b4b59e512e
Adam Zygadlewicz <adam@zygadlewicz.com>
2024-01-23 21:18:37 +0100

    added theme toggler

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import {  Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils"
+import { ThemeProvider } from "@/components/theme-provider"
+
 
 const fontSans = FontSans({ 
   subsets: ["latin"],
@@ -25,7 +27,17 @@ export default function RootLayout({
           "min-h-screen bg-background font-sans antialiased",
           fontSans.variable
         )
-      }>{children}</body>
+      }>
+        
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+          </body>
     </html>
   );
 }
